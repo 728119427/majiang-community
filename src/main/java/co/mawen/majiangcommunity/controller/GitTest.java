189c11ca23ad65a087e9592d1f98e87d9f66a563
728119427 <64735609+728119427@users.noreply.github.com>
2020-10-16 19:48:43 +0800

    Update GitTest.java

--- src/main/java/co/mawen/majiangcommunity/controller/GitTest.java
+++ src/main/java/co/mawen/majiangcommunity/controller/GitTest.java
@@ -3,5 +3,6 @@ package co.mawen.majiangcommunity.controller;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Add From Git!");
     }
 }
