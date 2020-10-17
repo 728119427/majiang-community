bcf3f81a4bf0aa43cea2663cf82b9c8e7012adc0
728119427 <72811947@qq.com>
2020-10-17 16:45:42 +0800

    delete GitTest.java

--- src/main/java/co/mawen/majiangcommunity/controller/GitTest.java
+++
@@ -1,8 +0,0 @@
-package co.mawen.majiangcommunity.controller;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("Hello,Git!");
-        System.out.println("Add From Git!");
-    }
-}
