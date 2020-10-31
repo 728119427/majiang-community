8704dde72f925b113c939a93e78719aa72e84344
728119427 <72811947@qq.com>
2020-10-31 21:55:12 +0800

    修改标签库，减少轮播图切换时间

--- src/main/java/co/mawen/majiangcommunity/cache/TagCache.java
+++ src/main/java/co/mawen/majiangcommunity/cache/TagCache.java
@@ -15,6 +15,19 @@ import java.util.stream.Collectors;
 public class TagCache {
     public static List<TagDTO> get() {
         List<TagDTO> tagDTOS = new ArrayList<>();
+
+
+        TagDTO game = new TagDTO();
+        game.setCategoryName("游戏");
+        game.setTags(Arrays.asList("炉石传说", "守望先锋", "暴雪", "英雄联盟", "绝地求生", "原神", "剑网3", "DNF", "CF", "CS:GO", "DoTA2", "自走棋", "剑网3", "冒险岛", "梦幻西游", "主机游戏", "单机", "3A大作", "qq飞车", "王者荣耀", "阴阳师", "和平精英", "手游", "网游", "页游", "桌游", "其他游戏"));
+        tagDTOS.add(game);
+
+        TagDTO entertainment = new TagDTO();
+        entertainment.setCategoryName("影音娱乐");
+        entertainment.setTags(Arrays.asList("火影忍者", "海贼王", "死神", "炎炎消防队", "期待在地下城邂逅有错吗", "请问您今天要来点兔子吗", "魔法科高校的劣等生", "鬼灭之刃", "罪恶王冠", "神奇宝贝", "数码宝贝", "进击的巨人", "希德尼娅的骑士", "来自新世界", "弹丸论破", "尸鬼", "粗点心战争", "钢之炼金术师", "妖精的尾巴", "家庭教师", "银魂", "野良神", "\n" +
+                "新世纪福音战士", "网球王子", "命运石之门", "全职猎人", "反叛的鲁鲁修", "Fate/Zero","其他动漫"));
+        tagDTOS.add(entertainment);
+
         TagDTO program = new TagDTO();
         program.setCategoryName("开发语言");
         program.setTags(Arrays.asList("java", "php", "css", "html", "html5", "javascript", "node.js", "python", "c++", "c", "golang", "objective-c", "typescript", "shell", "swift", "c#", "sass", "ruby", "bash", "less", "asp.net", "lua", "scala", "coffeescript", "actionscript", "rust", "erlang", "perl"));
@@ -43,7 +56,7 @@ public class TagCache {
 
         TagDTO others=new TagDTO();
         others.setCategoryName("其他");
-        others.setTags(Arrays.asList("游戏","动漫","漫画","巨乳","超级","大奶子","宝贝","神奇","比卡丘","大奶瓜","老夫","神奇宝贝","神","广告","招聘"));
+        others.setTags(Arrays.asList("游戏","动漫","漫画","巨乳","超级","宝贝","神奇","比卡丘","老夫","神","广告","招聘","电视剧","美剧","日剧","韩剧"));
         tagDTOS.add(others);
         return tagDTOS;
     }
